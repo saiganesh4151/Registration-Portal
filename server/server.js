463ac18a0421a9361f042960488b8b295cfef9ac
SaiGanesh4151 <saiganeshambati000@gmail.com>
2025-08-21 20:19:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://registration-portal-pi.vercel.app',
         credentials: true
     }
 ));
